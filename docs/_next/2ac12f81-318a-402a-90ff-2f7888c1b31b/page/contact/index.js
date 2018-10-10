c806e42a0f59483aad217c53d1f3cee727571878
bartoszgromek <bartgromek@gmail.com>
2018-10-10 21:08:30 +0200

    First Commit

---
+++ docs/_next/2ac12f81-318a-402a-90ff-2f7888c1b31b/page/contact/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/contact', function() {
+            var comp = module.exports=webpackJsonp([6],{245:function(e,t,u){e.exports=u(246)},246:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var o=u(1),l=n(o),r=u(49),a=n(r);t.default=function(){return l.default.createElement(a.default,null,"Contact Page")}}},[245]);
+            return { page: comp.default }
+          })
+        

031068856de3971cb7abdad166b5287be6828b06
bartoszgromek <bartgromek@gmail.com>
2018-10-10 22:13:23 +0200

    comments2

--- layouts/Main.js
+++ layouts/Main.js
@@ -23,12 +23,13 @@ export default ({ children, title = 'This is the default title' }) => (
         /**
         *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
         *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+        /*
         var disqus_config = function () {
-          this.page.url = 'https://git.heroku.com/next-go.git';  // Replace PAGE_URL with your page's canonical URL variable
-          this.page.identifier = 'xd'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
         };
-
-        (function() {
+        */
+        (function() { // DON'T EDIT BELOW THIS LINE
         var d = document, s = d.createElement('script');
         s.src = 'https://https-next-go-herokuapp-com.disqus.com/embed.js';
         s.setAttribute('data-timestamp', +new Date());

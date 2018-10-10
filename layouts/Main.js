843fb4e0b2524ecce6564c77bcef717b35523b8e
bartoszgromek <bartgromek@gmail.com>
2018-10-10 22:11:19 +0200

    comments

--- layouts/Main.js
+++ layouts/Main.js
@@ -17,6 +17,26 @@ export default ({ children, title = 'This is the default title' }) => (
     </main>
 
     <Footer>
+      <div id="disqus_thread"></div>
+      <script>
+
+        /**
+        *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+        *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+        var disqus_config = function () {
+          this.page.url = 'https://git.heroku.com/next-go.git';  // Replace PAGE_URL with your page's canonical URL variable
+          this.page.identifier = 'xd'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        };
+
+        (function() {
+        var d = document, s = d.createElement('script');
+        s.src = 'https://https-next-go-herokuapp-com.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+      })();
+      </script>
+      <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
       Footer
     </Footer>
   </Wrapper>

9ea9fe3e12c423cc96927ec854ac2b3249c0c385
bartoszgromek <bartgromek@gmail.com>
2018-10-11 19:17:31 +0200

    work in progress

--- layouts/Main.js
+++ layouts/Main.js
@@ -17,15 +17,9 @@ export default ({ children, title = 'This is the default title' }) => (
     </main>
 
     <Footer>
-      <div id="disqus_thread"></div>
       <script>
-          var d = document, s = d.createElement('script');
-          s.src = 'https://https-next-go-herokuapp-com.disqus.com/embed.js';
-          s.setAttribute('data-timestamp', +new Date());
-          (d.head || d.body).appendChild(s);
+        console.log('yo');
       </script>
-      <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-
       Footer
     </Footer>
   </Wrapper>
